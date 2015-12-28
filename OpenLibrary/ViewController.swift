7013e1c2ef045a0dbc65ab3903f1051ba7f41d6c
chintoz <jacintomena@gmail.com>
2015-12-28 18:40:48 +0100

    Cambios para procesar el JSON, y mostrar el titulo, los autores y la portada en el caso de que exista.

--- OpenLibrary/ViewController.swift
+++ OpenLibrary/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultado: UITextView!
     
+    @IBOutlet weak var titulo: UILabel!
+    
+    @IBOutlet weak var autores: UILabel!
+    
+    @IBOutlet weak var portada: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,19 +33,22 @@ class ViewController: UIViewController {
     @IBAction func limpiar(sender: AnyObject) {
         // Se borra el resultado
         self.isbn.text = nil
-        self.resultado.text = nil
+        self.autores.text = nil
+        self.titulo.text = nil
+        self.portada.image = nil
     }
     
     @IBAction func buscar(sender: AnyObject) {
         
         if (isbn.text != nil && isbn.text!.isEmpty) {
-            self.resultado.text = nil
+            self.titulo.text = nil
+            self.autores.text = nil
+            self.portada.image = nil
         } else {
             // Se realiza la petición
             self.request()
         }
     }
-    
     
     func request() {
         let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + self.isbn.text!
@@ -50,10 +59,68 @@ class ViewController: UIViewController {
             resultado.text = "Se ha producido un error en la conexión con el servidor. Pruebe más tarde."
             
         } else {
-            let texto = NSString(data:datos!, encoding:NSUTF8StringEncoding)
             
-            // Se muestra el resultado de la petición
-            resultado.text = texto as! String
+            
+            // Se convierte el resultado en un JSON para mostrarlo
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                if (json["ISBN:" + self.isbn.text!] == nil) {
+                    throw NoDataError.NoDataFound
+                }
+                let dicLibro = json["ISBN:" + self.isbn.text!] as! NSDictionary
+                let valueTitulo = dicLibro["title"] as! NSString as String
+                
+                // Asignamos el valor del título al campo correspondiente
+                self.titulo.text = valueTitulo
+                
+                // Se saca la información de los autores
+                var valueAutores = "Desconocido"
+                
+                if dicLibro["authors"] != nil {
+                    let dicAutores = dicLibro["authors"] as! NSArray
+                    
+                    if dicAutores.count > 0 {
+                        valueAutores = "";
+                        var primero = true
+                        for (var j = 0; j < dicAutores.count; j++) {
+                            if (primero) {
+                                valueAutores = (dicAutores[j] as! NSDictionary)["name"] as! NSString as String
+                                primero = false
+                            } else {
+                                valueAutores = valueAutores + "\n" + ((dicAutores[j] as! NSDictionary)["name"] as! NSString as String)
+                            }
+                        }
+                    }
+                }
+                
+                // Asignamos el valor de los autores
+                self.autores.text = valueAutores
+                
+                // Sacamos la portada si existe
+                if dicLibro["cover"] != nil {
+                    let dicCover = dicLibro["cover"] as! NSDictionary
+                    if (dicCover["large"] != nil) {
+                        let urlString = dicCover["large"] as! NSString as String
+                        let imgURL: NSURL = NSURL(string: urlString)!
+                        let request: NSURLRequest = NSURLRequest(URL: imgURL)
+                        NSURLConnection.sendAsynchronousRequest(
+                            request, queue: NSOperationQueue.mainQueue(),
+                            completionHandler: {(response: NSURLResponse?,data: NSData?,error: NSError?) -> Void in
+                                if error == nil {
+                                    self.portada.image = UIImage(data: data!)
+                                }
+                        })
+                    }
+                }
+                
+            } catch NoDataError.NoDataFound {
+                limpiar(self)
+            } catch {
+                print("Se ha producido un error no controlado")
+            }
+            
+            
+        
         }
         
     }
@@ -68,6 +135,12 @@ class ViewController: UIViewController {
         
         // Se lanza la búsqueda en el enter
         self.buscar(sender)
+    }
+    
+    
+    internal enum NoDataError : ErrorType {
+        case NoDataFound
+        case NoAuthorFound
     }
 }
 
